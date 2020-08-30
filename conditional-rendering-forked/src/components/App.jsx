2380e9a2dfa7393759c95852067303a84b475876
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-08-29 18:27:22 -0700

    Finish conditional rendering

--- conditional-rendering-forked/src/components/App.jsx
+++ conditional-rendering-forked/src/components/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
+import Login from "./Login"
+
+let isLoggedIn = false;
+
+let currentTime = new Date().getHours();
+
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+  return  <div className="container">{
+    /* isLoggedIn ? <h1>Hello</h1> : <Login /> */
+    (currentTime > 17) ? <h1>Why are you still working?</h1> : <h1>Work hard!</h1>
+  }</div>;
 }
 
 export default App;
