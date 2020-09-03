201015b373e5158dc6491ee4bc9926cf315d0a8d
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-09-03 11:15:06 -0700

    Finish event handling

--- event-handling-in-react/src/components/App.jsx
+++ event-handling-in-react/src/components/App.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 
-function App() {
+const App = () => {
+
+  const [headingText, setHeadingText] = useState("Hello world!");
+  const [isMousedOver, setMousedOver] = useState(false); 
+  
+
+  
+ 
+  const handleClick = () => {
+    setHeadingText("Submitted!");
+  };
+
+  const handleMousedOver = () => {
+    setMousedOver(true);
+  };
+
+  const handleMousedOut = () => {
+    setMousedOver(false);
+  };
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button onClick={handleClick} 
+      style={{ backgroundColor: isMousedOver ? "black" : "white"}}
+      onMouseOver={handleMousedOver}
+      onMouseOut={handleMousedOut}
+      >Submit</button>
     </div>
   );
 }
