98b20d36412abd129bbf5dc82d2bf31286f1ad12
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-09-04 08:38:50 -0700

    Finish practice changing complex state

--- changing-complex-state/src/components/App.jsx
+++ changing-complex-state/src/components/App.jsx
@@ -1,13 +1,46 @@
-import React from "react";
+import React, {useState} from 'react';
 
 function App() {
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [headingFText, setFHeading] = useState("");
+  const [headingLText, setLHeading] = useState("");
+
+
+  const handleFChange = (event) => { 
+    setFirstName(event.target.value);
+  };
+
+  const handleLChange = (event) => {
+    setLastName(event.target.value);
+  }
+
+
+  const handleClick = (event) => {
+    setFHeading(firstName);
+    setLHeading(lastName);
+    event.preventDefault();
+  };
+
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {headingFText} {" "} {headingLText}</h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <button>Submit</button>
+        <input 
+        onChange={handleFChange} 
+        name="fName" 
+        placeholder="First Name" 
+        value={firstName}
+        />
+        <input 
+        onChange={handleLChange}
+        name="lName" 
+        placeholder="Last Name" 
+        value={lastName}
+        />
+        <button onClick={handleClick}>Submit</button>
       </form>
     </div>
   );
