8860f61cbbcee29bd8a453318b2c7323fb0d47e3
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-09-10 07:19:53 -0700

    Finish es6 spread operator practice

--- es6-spread-operator-practice-forked/src/components/App.jsx
+++ es6-spread-operator-practice-forked/src/components/App.jsx
@@ -1,23 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
 
-  const [inputText, setInputaText] = useState();
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
   
+  const handleChange = (event) => {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  };
+
+  const handleItems = () => {
+    setItems ((prevItems) => {
+      return [...prevItems, inputText ];
+    });
+    setInputText("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" value={inputText} onChange={handleChange}/>
+        <button onClick={handleItems}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map((item) => <li>{item}</li>)};
         </ul>
       </div>
     </div>
