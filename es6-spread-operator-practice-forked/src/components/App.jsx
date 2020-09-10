4b77d56d4a581bbb5dd06c25f27ab303aaa7222a
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-09-10 06:49:31 -0700

    Es6 spread operator practice

---
+++ es6-spread-operator-practice-forked/src/components/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function App() {
+
+  const [inputText, setInputaText] = useState();
+  
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input type="text" />
+        <button>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          <li>A Item </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
